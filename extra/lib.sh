105a7fbb166afbaa718dffa8e7f8a8faf3ad3b9f
David Decker <xfaith@gmail.com>
2023-08-12 03:03:17 +0000

    Updated Package.Json -Babel-eslint ^7 -Eslint ^3 -Grunt-browserfiy - ^6

--- extra/lib.sh
+++ extra/lib.sh
@@ -54,7 +54,7 @@ function docker_install(){
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   sudo apt-get update
-  sudo apt-get install docker-ce
+  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 }
 
 
